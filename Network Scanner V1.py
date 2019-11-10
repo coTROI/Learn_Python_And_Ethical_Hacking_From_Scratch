e9ab30d402731b7d6364629404d525d69afb9d3c
cotroi <inhaltschriftsteller@gmail.com>
2019-11-10 07:29:31 -0500

    #Starting the VCS

--- Network Scanner V1.py
+++ Network Scanner V1.py
@@ -1,1 +1,10 @@
-print("Hello")
+import scapy.all as scapy
+
+# just using arping
+
+
+def scan(ip):
+    scapy.arping(ip)
+
+
+scan("192.168.0.1/24")
---
+++ Network Scanner V2.py
@@ -0,0 +1,14 @@
+import scapy.all as scapy
+
+# just using arping
+
+print("Use the scan function now")
+
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether()   #Creates an ethernet
+    scapy.ls(scapy.Ether())
+
+
+scan("192.168.0.1")
