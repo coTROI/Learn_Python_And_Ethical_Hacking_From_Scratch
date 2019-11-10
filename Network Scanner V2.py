5d213b9754eec5fde2037854f34b452d76170773
cotroi <inhaltschriftsteller@gmail.com>
2019-11-10 07:40:05 -0500

    Using Ethernet frames

--- Network Scanner V2.py
+++ Network Scanner V2.py
@@ -7,7 +7,7 @@ print("Use the scan function now")
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether()   #Creates an ethernet
+    broadcast = scapy.Ether()   # Creates an ethernet frame
     scapy.ls(scapy.Ether())
 
 
